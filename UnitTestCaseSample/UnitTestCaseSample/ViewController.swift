7c6ca82382bc9703556a39f655c88285b26283a4
Monica deswal <Monica>
2020-09-10 10:39:45 +0530

    basic view controller set up

---
+++ UnitTestCaseSample/UnitTestCaseSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestCaseSample
+//
+//  Created by Monica Deswal on 10/09/20.
+//  Copyright © 2020 Monica Deswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
